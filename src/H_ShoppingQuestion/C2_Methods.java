706551cd9f509dde2b83116a9b8ee46a0ae79b30
aliyuksel64 <ayuksel6400@gmail.com>
2023-09-02 19:02:05 +0300

    Arrays ang fori loop

---
+++ src/H_ShoppingQuestion/C2_Methods.java
@@ -0,0 +1,103 @@
+package H_ShoppingQuestion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+
+public class C2_Methods {
+    static Scanner scan = new Scanner(System.in);
+    static String[] days = {"Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+    static ArrayList<String> gunlukKazanclar = new ArrayList<String>(7);
+
+
+
+    public static void main(String[] args) {
+
+        int sayac = 0;
+        while (sayac < days.length) {
+            System.out.print(days[sayac] + " gününün kazancını girin: ");
+            String kazanc = scan.next();
+            gunlukKazanclar.add(kazanc);
+            sayac++;
+        }
+        System.out.println("Günlük kazançlar Listesi: " + gunlukKazanclar);
+
+        System.out.println("Yapmak istediğiniz işlemi seçin: \n" +
+                "Haftalık Ortalama için: 1'e basın.\n" +
+                "Haftalık ortalama üstünde kazanılan günler için: 2'ye basın.\n" +
+                "Haftalık ortalama altında kazanılan günler için: 3'e basın...");
+        int secim = scan.nextInt();
+        switch (secim) {
+            case 1:
+                getOrtalamaKazanc();
+                break;
+            case 2:
+                getOrtalamaninUstundeKazancGunleri();
+                break;
+            case 3:
+                getOrtalamaninAltindaKazancGunleri();
+                break;
+            default:
+                System.out.println("Yanlış tercih yaptınız.");
+
+        }
+
+
+    }
+
+    public static int getOrtalamaKazanc() {
+        int haftalikToplam = 0;
+        int haftalikOrtalama = 0;
+        for (int i = 0; i < days.length; i++) {
+            haftalikToplam += Integer.parseInt(gunlukKazanclar.get(i));
+            haftalikOrtalama = haftalikToplam / 7;
+
+        }
+        System.out.println("Haftalık toplam kazanç: " + haftalikToplam + "\n" +
+                "Haftalık ortalama kazanç: " + haftalikOrtalama);
+        return haftalikOrtalama;
+    }
+
+    public static void getOrtalamaninUstundeKazancGunleri() {
+        int ortalama = getOrtalamaKazanc();
+
+        ArrayList<String> ortalamaUstuKazancGunleri =new ArrayList<>();
+             for (int i = 0; i < days.length; i++) {
+                 int kazanc =Integer.parseInt(gunlukKazanclar.get(i));
+                if (kazanc>ortalama){
+                     ortalamaUstuKazancGunleri.add(days[i]);
+                 }
+             }
+
+
+        System.out.println("Ortalama üstünde kazanılan günler: "+ortalamaUstuKazancGunleri);
+    }
+
+
+ public static void getOrtalamaninAltindaKazancGunleri(){
+     int ortalama = getOrtalamaKazanc();
+
+     ArrayList<String> ortalamaAltiKazancGunleri =new ArrayList<>();
+     for (int i = 0; i < days.length; i++) {
+         int kazanc =Integer.parseInt(gunlukKazanclar.get(i));
+         if (kazanc<ortalama){
+             ortalamaAltiKazancGunleri.add(days[i]);
+         }
+     }
+     System.out.println("Ortalama altında kazanılan günler: "+ortalamaAltiKazancGunleri);
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+}
